2252d15e033e2aa401f5d5b43797e77921110126
Danylo <ddssoolloovvkkoo4400@gmail.com>
2023-12-12 15:26:12 +0200

    Урок 3.3 Виконано

--- Pool8Preview/Assets/Game/Scripts/BallStopper.cs
+++ Pool8Preview/Assets/Game/Scripts/BallStopper.cs
@@ -1,45 +1,73 @@
-using System.Globalization;
 using UnityEngine;
 
 public class BallStopper : MonoBehaviour
 {
-    [SerializeField] GameObject[] ballRb;
     [SerializeField] GameObject cue;
-    [SerializeField] int numberOfBalls;
     [SerializeField] float minSpeed;
+    [SerializeField] int stopSpeed;
+
+    public int numberOfBalls;
+    public GameObject[] ballsRb;
+    bool checkBalls = false;
     private void Start()
     {
-        ballRb = GameObject.FindGameObjectsWithTag("Ball");
+        ballsRb = GameObject.FindGameObjectsWithTag("Ball");
     }
     void Update()
     {
-        BallSpeedCheckAndStop();
-        // Show cue:
-        //for (int i = 0; i < 16; i++)
-        //{
-        //    float speed = ballRb[i].GetComponent<Rigidbody>().velocity.magnitude;
-        //    if (speed == 0)
-        //    {
-        //        cue.SetActive(true);
-        //    }
-        //}
-
+        CheckBallSpeedAndShowCue();
     }
 
-    void BallSpeedCheckAndStop ()
+    void CheckBallSpeedAndShowCue()
+    {
+        CheckBallSpeed();
+        ShowCue();
+    }
+
+    void CheckBallSpeed()
     {
         for (int i = 0; i < numberOfBalls; i++)
-        {          
-            if (BallChecking(i))
+        {
+            float speed = ballsRb[i].GetComponent<Rigidbody>().velocity.magnitude;
+            if (speed == stopSpeed)
             {
-                ballRb[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                checkBalls = true;
+            }
+            else
+            {
+                checkBalls = false;
+                break;
             }
         }
     }
 
-   bool BallChecking(int i)
+    void ShowCue()
     {
-        float speed = ballRb[i].GetComponent<Rigidbody>().velocity.magnitude; // find gameobject`s speed.
+        if (checkBalls)
+        {
+            cue.SetActive(true);
+        }
+    }
+
+    public void BallSpeedCheckAndStop()
+    {
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            StopBalls(i);
+        }
+    }
+
+    void StopBalls(int i)
+    {
+        if (BallChecking(i))
+        {
+            ballsRb[i].GetComponent<Rigidbody>().Sleep();
+        }
+    }
+
+    bool BallChecking(int i)
+    {
+        float speed = ballsRb[i].GetComponent<Rigidbody>().velocity.magnitude; // find gameobject`s speed.
         return speed < minSpeed;
     }
 }
--- Pool8Preview/Assets/Game/Scripts/CueBlow.cs
+++ Pool8Preview/Assets/Game/Scripts/CueBlow.cs
@@ -1,31 +1,68 @@
+using System.Collections;
 using UnityEngine;
 
 public class CueBlow : MonoBehaviour
 {
-    [SerializeField] Transform ballPoint;
-    [SerializeField] float power = 1.0f;
+    [SerializeField] float power;
     [SerializeField] GameObject cue;
-    
+    [SerializeField] int waitForSeconds;
+    [SerializeField] Transform ballPoint;
+    [SerializeField] int mouseButton;
+    [SerializeField] int stopSpeed;
+
+    GameObject ballStopper;
     Rigidbody cueRb;
     private void Start()
     {
         cueRb = GetComponent<Rigidbody>();
+        ballStopper = GameObject.FindGameObjectWithTag("Ball Stopper");
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(mouseButton))
         {
             BlowBall();
+            StartCoroutine(BallStop());
         }
     }
 
     void BlowBall()
     {
-        //Hide cue: 
-        //cue.SetActive(false); 
+        HideCue();
         Vector3 hitDirection = ballPoint.transform.right;
         hitDirection = new Vector3(hitDirection.x, 0, hitDirection.z).normalized;
         cueRb.AddForce(hitDirection * power, ForceMode.Impulse);
+    }
+
+    void HideCue()
+    {
+        cue.SetActive(false);
+    }
+
+    IEnumerator BallStop()
+    {
+        yield return new WaitForSeconds(waitForSeconds);
+        ballStopper.GetComponent<BallStopper>().BallSpeedCheckAndStop();
+
+        for (int i = 0; i < NumberOfBalls(); i++)
+        {
+            if (CheckBallSpeed(i))
+            {
+                yield return new WaitForSeconds(waitForSeconds);
+                ballStopper.GetComponent<BallStopper>().BallSpeedCheckAndStop();
+            }
+        }
+
+    }
+
+    bool CheckBallSpeed(int i)
+    {
+        return ballStopper.GetComponent<BallStopper>().ballsRb[i].GetComponent<Rigidbody>().velocity.magnitude != stopSpeed;
+    }
+
+    int NumberOfBalls()
+    {
+        return ballStopper.GetComponent<BallStopper>().numberOfBalls;
     }
 }
